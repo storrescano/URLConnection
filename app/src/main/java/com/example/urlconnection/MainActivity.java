29177b1bbf45b42e33c8da232f50057f3602ac6a
storrescano <storres@cifpfbmoll.eu>
2022-12-16 18:00:34 +0100

    Problemas tecnicos

--- app/src/main/java/com/example/urlconnection/MainActivity.java
+++ app/src/main/java/com/example/urlconnection/MainActivity.java
@@ -2,60 +2,128 @@ package com.example.urlconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String DEBUG_TAG = "HttpExample";
+    private EditText urlText;
+    private TextView textView;
+    private Button download;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        urlText = findViewById(R.id.URLInsert);
+        textView = findViewById(R.id.web);
+        download = findViewById(R.id.download);
 
-        EditText url = findViewById(R.id.URLInsert);
-        Button download = findViewById(R.id.download);
 
-
-        download.setOnClickListener(v -> {
-            String stringUrl = url.getText().toString();
-            urlConnection(stringUrl);
+        download.setOnClickListener(v-> {
+            String stringUrl = urlText.getText().toString();
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                new DownloadWebpageText().execute(stringUrl);
+            } else {
+                textView.setText("No network connection available.");
+            }
         });
     }
 
+    private class DownloadWebpageText extends AsyncTask {
 
-    public void urlConnection(String stringUrl) {
-        // Especifica la URL a la que quieres conectarte
-        try {
-            // Crea un objeto URL a partir de la URL especificada
-            URL url = new URL(stringUrl);
+        @Override
+        protected String doInBackground(String... urls) {
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-
-            InputStream inputStream = connection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
             }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+        }
 
-            String text = result.toString();
-            System.out.println(text);
-        } catch (IOException e) {
-            e.printStackTrace();
+        @Override
+        protected Object doInBackground(Object[] objects) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
         }
     }
 
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+
 }
+
+
+
 
